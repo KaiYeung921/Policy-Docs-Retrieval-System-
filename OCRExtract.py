3a240ab4a1c7344ef8a9cecc8e1a4b112d3f3396
Kai Yeung <kyeung921@gmail.com>
2026-02-23 23:01:35 -0800

    Changed OCR extract to name documents properly, change langExtract to a simpler schema, and added another test cast'

--- OCRExtract.py
+++ OCRExtract.py
@@ -3,6 +3,8 @@
 from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
 import sys
+from pathlib import Path
+
 
 if len(sys.argv) != 2:
     print("Usage: ./ocr_script.py <input_pdf>")
@@ -13,12 +15,15 @@ input_pdf = sys.argv[1]
 
 model = ocr_predictor(pretrained=True)
 
-doc = DocumentFile.from_pdf("LegalDocTest.pdf")
+doc = DocumentFile.from_pdf(input_pdf)
 result = model(doc)
 
 full_text=result.render()
 
-with open("FullDocExtracted.txt", "w", encoding="utf-8") as f:
+input_path = Path(input_pdf)
+output_filename = input_path.stem + "_extracted.txt"
+
+with open(output_filename, "w", encoding="utf-8") as f:
     f.write(full_text)
 
-print("OCR extraction complete. Output saved to FullDocExtracted.txt")
+print("OCR extraction complete. Output")
